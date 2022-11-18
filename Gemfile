f613e5370b8b8d2df2bc0e5b8870cc4e1214b8f9
Michael Epelboim <sdrmike@outlook.com>
2022-11-18 09:29:14 +0800

    scaffold restaurants

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.1.1"
 
 gem "simple_form", github: "heartcombo/simple_form"
+gem "faker"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4"
@@ -27,7 +28,7 @@ gem "turbo-rails"
 gem "stimulus-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
-gem "jbuilder"
+# gem "jbuilder"
 
 # Use Redis adapter to run Action Cable in production
 gem "redis", "~> 4.0"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'deleteing all previous restaurants'
+Restaurant.destroy_all
+
+puts 'creating new restaurants:'
+
+100.times do
+  r = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+
+  puts "Created restaurant #{r.id} - #{r.name}"
+end
+
+puts 'finished creating restaurants'
